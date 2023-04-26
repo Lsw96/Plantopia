55441eaca5fabfa8a39057e69bd33671ee09d6ed
Leeseongwoo <tjddn1007@naver.com>
2023-04-26 18:33:43 +0900

    main

--- assets/js/index.js
+++ assets/js/index.js
@@ -139,7 +139,10 @@ ctx = cb.getContext('2d');
 ctx.strokeStyle = 'green';
 ctx.beginPath();
 ctx.moveTo(75, 25);
-ctx.quadraticCurveTo(25, 25, 25, 62)
-ctx.quadraticCurveTo(25,100, 50, 100)
-ctx.quadraticCurveTo(50,120, 30, 125)
+ctx.quadraticCurveTo(25, 25, 25, 62.5);
+ctx.quadraticCurveTo(25, 100, 50, 100);
+ctx.quadraticCurveTo(50, 120, 30, 125);
+ctx.quadraticCurveTo(60, 120, 70, 100);
+ctx.quadraticCurveTo(125, 100, 125, 62.5);
+ctx.quadraticCurveTo(125, 25, 75, 25);
 ctx.stroke();
