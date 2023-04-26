7630eeb0cd1bc849896b900e0ac57df6396eadd0
Leeseongwoo <tjddn1007@naver.com>
2023-04-26 18:53:28 +0900

    main

--- assets/js/index.js
+++ assets/js/index.js
@@ -86,9 +86,9 @@
 // });
 
 //--------------------------------------------------------------------------
-// 면 그리기
-const canvas = document.querySelector('.canvas');
-const context = canvas.getContext('2d');
+// // 면 그리기
+// const canvas = document.querySelector('.canvas');
+// const context = canvas.getContext('2d');
 
 context.fillStyle = 'red';
 context.fillRect(50,50,100,100);
@@ -146,3 +146,41 @@ ctx.quadraticCurveTo(60, 120, 70, 100);
 ctx.quadraticCurveTo(125, 100, 125, 62.5);
 ctx.quadraticCurveTo(125, 25, 75, 25);
 ctx.stroke();
+
+const canvas = document.querySelector("canvas4");
+const ctx = canvas.getContext("2d");
+let raf;
+
+const ball = {
+  x: 100,
+  y: 100,
+  vx: 5,
+  vy: 2,
+  radius: 25,
+  color: "blue",
+  draw() {
+    ctx.beginPath();
+    ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, true);
+    ctx.closePath();
+    ctx.fillStyle = this.color;
+    ctx.fill();
+  },
+};
+
+function draw() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  ball.draw();
+  ball.x += ball.vx;
+  ball.y += ball.vy;
+  raf = window.requestAnimationFrame(draw);
+}
+
+canvas.addEventListener("mouseover", (e) => {
+  raf = window.requestAnimationFrame(draw);
+});
+
+canvas.addEventListener("mouseout", (e) => {
+  window.cancelAnimationFrame(raf);
+});
+
+ball.draw();
