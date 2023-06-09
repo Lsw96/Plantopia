7b475761c16b81e3d467fd73acc64b34c0f8a420
Leeseongwoo <tjddn1007@naver.com>
2023-06-09 14:51:26 +0900

    main

---
+++ assets/js/common.js
@@ -0,0 +1,22 @@
+let observer = new IntersectionObserver((e)=>{
+    e.forEach((leftDoor)=>{
+        if(leftDoor.intersectionRatio > 0){
+            leftDoor.target.style.left = -35 + "%";
+        } else {
+            leftDoor.target.style.left = 5 + "%";
+        }
+    });
+});
+let observers = new IntersectionObserver((e)=>{
+    e.forEach((leftDoor)=>{
+        if(leftDoor.intersectionRatio > 0){
+            leftDoor.target.style.right = -35 + "%";
+        } else {
+            leftDoor.target.style.right = 5 + "%";
+        }
+    });
+});
+let leftDoor = document.querySelectorAll('img')
+let rightDoor = document.querySelectorAll('.elevator > img')
+observer.observe(leftDoor[17])
+observers.observe(rightDoor[3])
