ac980e9186b0a7d25d7153460c97b37829a5d920
Leeseongwoo <tjddn1007@naver.com>
2023-06-10 04:43:25 +0900

    ele

--- assets/js/common.js
+++ assets/js/common.js
@@ -1,22 +1,40 @@
-// let observer = new IntersectionObserver((e)=>{
-//     e.forEach((leftDoor)=>{
-//         if(leftDoor.intersectionRatio > 0){
-//             leftDoor.target.style.left = -35 + "%";
-//         } else {
-//             leftDoor.target.style.left = 5 + "%";
-//         }
-//     });
-// });
-// let observers = new IntersectionObserver((e)=>{
-//     e.forEach((leftDoor)=>{
-//         if(leftDoor.intersectionRatio > 0){
-//             leftDoor.target.style.right = -35 + "%";
-//         } else {
-//             leftDoor.target.style.right = 5 + "%";
-//         }
-//     });
-// });
-// let leftDoor = document.querySelectorAll('img')
-// let rightDoor = document.querySelectorAll('.elevator > img')
-// observer.observe(leftDoor[17])
-// observers.observe(rightDoor[3])
+window.addEventListener('scroll', ()=>{
+    var scrolls = parseInt(window.scrollY);
+    console.log(scrolls);
+    // 1F
+    if( scrolls > 1199) {
+        document.querySelectorAll('#left-door')[0].style.left = "0%";
+        document.querySelectorAll('#right-door')[0].style.right = "-100%";
+    } else if ( scrolls > 300) {
+        document.querySelectorAll('#left-door')[0].style.left = "-40%";
+        document.querySelectorAll('#right-door')[0].style.right = "-140%";
+    }
+    else {
+        document.querySelectorAll('#left-door')[0].style.left = "0%";
+        document.querySelectorAll('#right-door')[0].style.right = "-100%";
+    }
+    // 2F
+    if( scrolls > 6000) {
+        document.querySelectorAll('#left-door')[1].style.left = "0%";
+        document.querySelectorAll('#right-door')[1].style.right = "-100%";
+    } else if ( scrolls > 4900) {
+        document.querySelectorAll('#left-door')[1].style.left = "-40%";
+        document.querySelectorAll('#right-door')[1].style.right = "-140%";
+    }
+    else {
+        document.querySelectorAll('#left-door')[1].style.left = "0%";
+        document.querySelectorAll('#right-door')[1].style.right = "-100%";
+    }
+    // 3F
+    if( scrolls > 9700) {
+        document.querySelectorAll('#left-door')[2].style.left = "0%";
+        document.querySelectorAll('#right-door')[2].style.right = "-100%";
+    } else if ( scrolls > 8600) {
+        document.querySelectorAll('#left-door')[2].style.left = "-40%";
+        document.querySelectorAll('#right-door')[2].style.right = "-140%";
+    }
+    else {
+        document.querySelectorAll('#left-door')[2].style.left = "0%";
+        document.querySelectorAll('#right-door')[2].style.right = "-100%";
+    }
+});
