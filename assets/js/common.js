b333760cdc9f014c660fb2a1b4769cb75be0def1
Leeseongwoo <tjddn1007@naver.com>
2023-06-11 23:15:35 +0900

    main

--- assets/js/common.js
+++ assets/js/common.js
@@ -1,14 +1,23 @@
 window.addEventListener('scroll', ()=>{
 
-    var scrolls = parseInt(window.scrollY);
+    var scrollTop = window.scrollY;
+    var innerHeights = window.innerHeight;
+    var scrolls = parseInt(scrollTop / (innerHeights) * 100);
     console.log(scrolls);
+    // var body = document.body;
+    // var scH = body.scrollHeight;
+    // console.log(scH);
+    // console.log(parseInt(scrollTop + innerHeights + 1))
+    // console.log("result = " + parseInt(scrollTop / (innerHeights) * 100))
+
+
     const leftDoor = document.querySelectorAll('#left-door');
     const rightDoor = document.querySelectorAll('#right-door');
     // 1F
-    if( scrolls > 1199 ) {
+    if( scrolls > 171 ) {
         leftDoor[0].style.left = "0%";
         rightDoor[0].style.right = "-100%";
-    } else if ( scrolls > 300 ) {
+    } else if ( scrolls > 36 ) {
         leftDoor[0].style.left = "-40%";
         rightDoor[0].style.right = "-140%";
     }
@@ -17,10 +26,10 @@ window.addEventListener('scroll', ()=>{
         rightDoor[0].style.right = "-100%";
     }
     // 2F
-    if( scrolls > 7000 ) {
+    if( scrolls > 886 ) {
         leftDoor[1].style.left = "0%";
         rightDoor[1].style.right = "-100%";
-    } else if ( scrolls > 6200 ) {
+    } else if ( scrolls > 752 ) {
         leftDoor[1].style.left = "-40%";
         rightDoor[1].style.right = "-140%";
     }
@@ -29,10 +38,10 @@ window.addEventListener('scroll', ()=>{
         rightDoor[1].style.right = "-100%";
     }
     // 3F
-    if( scrolls > 9700 ) {
+    if( scrolls > 1420 ) {
         leftDoor[2].style.left = "0%";
         rightDoor[2].style.right = "-100%";
-    } else if ( scrolls > 8600 ) {
+    } else if ( scrolls > 1287 ) {
         leftDoor[2].style.left = "-40%";
         rightDoor[2].style.right = "-140%";
     }
@@ -43,14 +52,14 @@ window.addEventListener('scroll', ()=>{
     
     // 1F CONTENT CARD
     user = document.querySelectorAll(".pj-user-box");
-    if ( scrolls > 5600 ) {
+    if ( scrolls > 660 ) {
         user[0].style.top = "50px";
         user[0].style.opacity = "0";
         user[1].style.top = "50px";
         user[1].style.opacity = "0";
         user[2].style.top = "50px";
         user[2].style.opacity = "0";
-    } else if ( scrolls > 4900 ) {
+    } else if ( scrolls > 580 ) {
         user[0].style.top = "0";
         user[0].style.opacity = "1";
         user[1].style.top = "0";
@@ -69,7 +78,7 @@ window.addEventListener('scroll', ()=>{
 
     // 2F CONTENT WIREFRAME
     const frame = document.querySelectorAll(".frame > img");
-    if ( scrolls > 8800 ) {
+    if ( scrolls > 1121 ) {
         frame[0].style.left = "100px";
         frame[0].style.opacity = "1";
         frame[1].style.left = "350px";
@@ -92,7 +101,7 @@ window.addEventListener('scroll', ()=>{
     }
 
     const frames = document.querySelectorAll(".frames > img");
-    if ( scrolls > 9600 ) {
+    if ( scrolls > 1182 ) {
         frames[0].style.left = "0";
         frames[0].style.opacity = "1";
         frames[1].style.left = "250px";
@@ -114,4 +123,23 @@ window.addEventListener('scroll', ()=>{
         frames[4].style.opacity = "0";
     }
 
+    // 3F SERVICE PHONE
+    phone = document.querySelector('#home');
+    phoneText = document.querySelector('#home-text');
+    if( scrolls > 1890 ) {
+        phone.style.left = "-21%";
+        phone.style.opacity = "0";
+        phoneText.style.left = "40%";
+        phoneText.style.opacity = "0";
+    } else if ( scrolls > 1774 ) {
+        phone.style.left = "9%";
+        phone.style.opacity = "1";
+        phoneText.style.left = "52%";
+        phoneText.style.opacity = "1";
+    } else {
+        phone.style.left = "-21%";
+        phone.style.opacity = "0";
+        phoneText.style.left = "40%";
+        phoneText.style.opacity = "0";
+    }
 });
